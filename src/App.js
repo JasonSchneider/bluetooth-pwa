5d5b260874c83b66d7144a458c6068fcb16ab92c
Jason Schneider <jas.schneider@gmail.com>
2025-02-08 15:03:40 -0500

    Fixed Bluetooth UUIDs to lowercase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-const LED_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"; // Custom Service UUID
-const LED_CHARACTERISTIC_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"; // LED Characteristic UUID
+const LED_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"; // Lowercase
+const LED_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; // Lowercase
+
 
 
 function App() {
